ccf8b8eb085c0c2cc908badf571ac851ed42c8f3
Michael Kohl <citizen428@gmail.com>
2011-07-18 17:18:49 +0200

    New models for import and match

---
+++ lib/tupalo_api_client/import.rb
@@ -0,0 +1,12 @@
+class Import < APISmith::Smash
+
+  property :name
+  property :city
+  property :country
+  property :street
+  property :phone
+  property :website
+  property :locale
+  property :returntext
+
+end
---
+++ lib/tupalo_api_client/match.rb
@@ -0,0 +1,6 @@
+class Match < APISmith::Smash
+
+  property :url
+  property :changes
+
+end
