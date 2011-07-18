5858df10af388b6472187b4d464efa0b99774b0e
Michael Kohl <citizen428@gmail.com>
2011-07-18 13:34:02 +0200

    Resolved merge conflict

--- lib/tupalo_api_client.rb
+++ lib/tupalo_api_client.rb
@@ -1,11 +1,18 @@
 require 'api_smith'
+require 'uri'
+require 'tupalo_api_client/spot'
 
 class TupaloAPIClient
-  LANGUAGE = 'en'
-
   include APISmith::Client
   base_uri 'http://tupalo.com/'
-  endpoint "#{LANGUAGE}/api/easy/v1"
+
+  def initialize(opts = {})
+    options = {
+      :lang => 'en',
+      :token => ''}.merge(opts)
+
+    self.class.endpoint "#{options[:lang]}/api/easy/v1"
+  end
 
   def spots(opts={})
     get(parameterize(opts), :transform => Spot)
@@ -14,12 +21,10 @@ class TupaloAPIClient
   def match(opts={})
   end
 
-
   private
 
   def parameterize(params)
-    query = "?"
-    query + URI.escape(params.collect{|k,v| "#{k}=#{v}"}.join('&'))
+    "?#{URI.escape(h.map{|k,v| "#{k}=#{v}"}.join('&'))}"
   end
 
 end
--- tupalo_api_client.gemspec
+++ tupalo_api_client.gemspec
@@ -3,10 +3,11 @@ Gem::Specification.new do |s|
   s.version = '1.0.0'
   s.authors = ['Michael Kohl', 'Andreas Tiefenthaler']
   s.email = ['michi@tupalo.com', 'andy@tupalo.com']
-  s.summary = 'A convenient wrapper for the Tupalo.com API'
+  s.description = 'A convenient wrapper for the Tupalo.com API'
+  s.summary = s.description
   s.homepage = 'https://github.com/tupalo/tupalo_api_client'
-  s.require_paths = %w[lib lib/tupalo_api_client]
-  s.files = %w[lib/tupalo_api_client.rb ./LICENSE ./README]
+  s.require_paths = ['lib']
+  s.files = `git ls-files`.split("\n")
   s.add_dependency('api_smith', '~> 1.0.0')
   s.has_rdoc = false
 end
